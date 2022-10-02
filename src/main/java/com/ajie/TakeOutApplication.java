1151b8beae3fb37d0e94b9264c7110390c05edfb
ajie <1651588782@qq.com>
2022-10-02 11:06:22 +0800

    配置拦截器，拦截用户登录范围请求

--- src/main/java/com/ajie/TakeOutApplication.java
+++ src/main/java/com/ajie/TakeOutApplication.java
@@ -1,14 +1,18 @@
 package com.ajie;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author:hyj
  * @date:2022/10/1
  */
 
+@Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class TakeOutApplication {
     public static void main(String[] args) {
         SpringApplication.run(TakeOutApplication.class, args);
